21f20a870a42727e691e89a1490ebb87e3598db3
maunticat <16717868+MauntiCatCode@users.noreply.github.com>
2026-02-12 22:19:03 +0200

    fix user ids not showing up in the registry

--- src/ecs/api/discord.py
+++ src/ecs/api/discord.py
@@ -4,10 +4,10 @@ from ..utils import get_singleton_component
 from .labels import create_labeled_entity
 
 def get_user_entity(discord_id: int) -> int:
-    return get_singleton_component(EntityRegistry).maps[DiscordID][discord_id]
+    return get_singleton_component(EntityRegistry).maps[DiscordID][DiscordID(discord_id)]
 
 def create_user_entity(discord_id: int, *comps) -> tuple[int, Label]:
-    ent, label = create_labeled_entity(*comps)
+    ent, label = create_labeled_entity(*comps, DiscordID(discord_id))
     return ent, label
 
     
